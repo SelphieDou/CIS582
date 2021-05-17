7454753a03a8c2b44c7b15ade113ca4b1fcf1b44
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-05-17 21:19:10 +0800

    0517

---
+++ caesar.py
@@ -0,0 +1,27 @@
+
+
+def encrypt(key,plaintext):
+    ciphertext=""
+    #YOUR CODE HERE
+    for character in plaintext:
+        num = ord(character)
+        new_num = num + key
+        if new_num > 90:
+            new_num -= 26
+        ciphertext += chr(new_num)
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    #YOUR CODE HERE
+    for character in ciphertext:
+        num = ord(character)
+        new_num = num - key
+        if new_num < 65:
+            new_num += 26
+        plaintext += chr(new_num)
+    return plaintext
+
+
+if __name__ == "__main__":
+    print decrypt(1, encrypt(1, "ABC"))
